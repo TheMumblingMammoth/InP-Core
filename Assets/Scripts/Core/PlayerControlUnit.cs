ec8c6dafe9d5f9042bb6596a0ffa771206e29736
TheMumblingMammoth <anton.mamonov.golohvastogo@mail.ru>
2025-07-08 17:05:19 +0300

    first movement

--- Assets/Scripts/Core/PlayerControlUnit.cs
+++ Assets/Scripts/Core/PlayerControlUnit.cs
@@ -12,16 +12,17 @@ public class PlayerControlUnit : MonoBehaviour
     Vector2 direction;
     void FixedUpdate()
     {
-        transform.position = transform.position + (Vector3)direction * speed * (Input.GetKey(KeyCode.LeftShift) ? 2: 1)* Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + (Vector3)direction,
+                                     speed * (Input.GetKey(KeyCode.LeftShift) ? 2: 1)* Time.deltaTime);
     }
 
     void Update()
     {
         float x = 0, y = 0;
-        if (Input.GetKey(KeyCode.W)) y = 1;
-        if (Input.GetKey(KeyCode.S)) y = -1;
-        if (Input.GetKey(KeyCode.A)) x = -1;
-        if (Input.GetKey(KeyCode.D)) x = 1;
+        if (Input.GetKey(KeyCode.W)) y = 100;
+        if (Input.GetKey(KeyCode.S)) y = -100;
+        if (Input.GetKey(KeyCode.A)) x = -100;
+        if (Input.GetKey(KeyCode.D)) x = 100;
         direction = new Vector2(x, y);
     }
 
