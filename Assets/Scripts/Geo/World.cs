614cdc57ddc03a8eb377d135272103b446d42c80
TheMumblingMammoth <anton.mamonov.golohvastogo@mail.ru>
2025-07-16 12:50:50 +0300

    tiles pretry

--- Assets/Scripts/Geo/World.cs
+++ Assets/Scripts/Geo/World.cs
@@ -5,12 +5,17 @@ public class World : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    #region Tiles
+        
+    #endregion Tiles
 }
+
